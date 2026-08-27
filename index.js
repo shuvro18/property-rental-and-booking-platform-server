419005bf4ca5b5012a8899633166e059120a12fa
shuvro18 <shuvrobiswas404@gmail.com>
2026-08-27 08:02:48 +0600

    start making the server

---
+++ index.js
@@ -0,0 +1,57 @@
+const dns = require('node:dns');
+dns.setServers(['1.1.1.1', '1.0.0.1']); 
+
+const express = require("express");
+const dontenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion,  } = require("mongodb");
+dontenv.config();
+
+const uri = process.env.MONGO_URI;
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    credentials: true,
+    origin: [process.env.CLIENT_URL],
+  }),
+);
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+   
+
+
+   
+ 
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running fine!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
